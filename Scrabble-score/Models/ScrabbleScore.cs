71a3f275ea7e9444db84e9a8b88dba8891af144e
Connor Burgess <connorburgesscodes@gmail.com>
2021-02-25 14:17:34 -0800

    Co-authored-by: Tien Nguyen <tien96ng@gmail.com> Co-Authored-by: Brian Sturgis <sturujisu@gmail.com>

--- Scrabble-score/Models/ScrabbleScore.cs
+++ Scrabble-score/Models/ScrabbleScore.cs
@@ -4,16 +4,29 @@ namespace Scrabble.Models
 {
   public class ScrabbleScore
   {
-    public static Dictionary<int, List<string>> scrabbleScoreBoard = new Dictionary<int, List<string>>() {
-      {1, new List<string> {"A", "B"}}
-      // {"2", {}};
-      // {"3", {}};
-      // {"4", {}};
-      // {"5", {}};
-      // {"8", {}};
-      // {"10", {}};
-
+    public static Dictionary<int, string> scrabbleScoreBoard = new Dictionary<int, string>() {
+      {1, "aeioulnrst"},
+      {2, "dg"},
+      {3, "bcmp"},
+      {4, "fhvwy"},
+      {5, "k"},
+      {8, "jx"},
+      {10, "qz"}
     };
 
+    public static int WordScore(string word) {
+      int total = 0;
+      for (int i = 0; i < word.Length; i++) {
+        char tempWord = Char.ToLower(word[i]);
+        foreach(KeyValuePair<int, string> obj in scrabbleScoreBoard) {
+          // Console.WriteLine($"Key: {obj.Key} | Value: {obj.Value}");
+          bool contains = obj.Value.Contains(tempWord);
+          if(contains) {
+            total += obj.Key;
+          }
+        }
+      }
+      return total;
+    }
   }
 }
--- Scrabble-score/Program.cs
+++ Scrabble-score/Program.cs
@@ -7,7 +7,10 @@ namespace Scrabble
   public class Program
   {
     public static void Main(){
- 
+      Console.Write("Enter in a word for Scrabble: ");
+      string userInput = Console.ReadLine();
+      Console.ForegroundColor  = ConsoleColor.Green;
+      Console.WriteLine(ScrabbleScore.WordScore(userInput));
     }
   }
 }
